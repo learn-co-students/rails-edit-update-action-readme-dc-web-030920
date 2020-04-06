80bd42dab07520eaeb98c9bdd4c0be54d8b8c875
itacamba <itacamba@live.com>
2020-04-05 22:54:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :new, :show, :create, :edit, :update]
+  # get '/articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # get '/articles/:id', to: 'articles#show', as: :article 
+  # C FROM CRUD
+ 
+
+  # get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch '/articles/:id', to: 'articles#update'
 end
+# :new, :show, :create, :edit, :update
